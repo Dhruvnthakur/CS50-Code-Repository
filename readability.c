5c260fbaa02fd50bfb31be81389eee48678fb227
Dhruvnthakur <Dhruvnthakur@users.noreply.github.com>
2025-01-03 15:16:28 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -12,12 +12,12 @@ int main(void)
     strcpy(c1, t1);
 
     int t = 0; // temp variable for calculations
-    float sp = 0; //number of spaces in 100 words
+    float sp = 1; //number of spaces in 100 words
     int fs = 0; //number of full stops in 100 words
     float S = 0; //Is the average number of sentences per 100 words in the text
     float L = 0; //Is the average number of letters per 100 words in the text
     int index = 0;
-    for(int i = 0 ; i < strlen(c1); i++)
+    for(int i = 0 ; i < strlen(c1) && sp < 100 ; i++)
     {
         if(c1[i] == ' ')
         {
@@ -27,11 +27,14 @@ int main(void)
         {
             fs++;
         }
-        t++;
+        else
+        {
+            t++;
+        }
     }
-    L = (t)/sp ;
-    S = (fs)/sp ;
-    index = 5.88 * L - 0.296 * S - 15.8 ;
+    L = ((t)/(sp))*100.0 ;
+    S = ((fs)/(sp))*100.0 ;
+    index = 0.0588 * L - 0.296 * S - 15.8 ;
     if(index < 1)
     {
         printf("Before Grade 1\n");
