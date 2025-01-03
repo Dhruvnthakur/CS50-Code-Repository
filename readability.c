db44e68d83813c602fbe145d7f8658aada390c05
Dhruvnthakur <Dhruvnthakur@users.noreply.github.com>
2025-01-03 15:30:06 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -34,7 +34,7 @@ int main(void)
     }
     L = ((t)/(sp))*100.0 ;
     S = ((fs)/(sp))*100.0 ;
-    index = 0.0588 * L - 0.296 * S - 15.8 ;
+    index = (0.0588 * L)- (0.296 * S) - 15.8 ;
     if(index < 1)
     {
         printf("Before Grade 1\n");
