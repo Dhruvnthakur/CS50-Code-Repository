18d27c3e45160f13098d6b6bcc7cdf8a525e1c99
Dhruvnthakur <Dhruvnthakur@users.noreply.github.com>
2025-01-05 06:42:54 +0000

    automated commit by submit50

--- caesar.c
+++ caesar.c
@@ -10,14 +10,14 @@ char *ciphertext(char arr[], int n);
 
 int main(int argc, string argv[])
 {
-    if (argc!=2)
+    if (argc != 2)
     {
         printf("Usage:./caesar key\n");
         return 1;
     }
-    for(int i = 0; i<strlen(argv[1]);i++)
+    for (int i = 0; i < strlen(argv[1]); i++)
     {
-        if(!isdigit(argv[1][i]))
+        if (!isdigit(argv[1][i]))
         {
             printf("Usage:./caesar key\n");
             return 1;
