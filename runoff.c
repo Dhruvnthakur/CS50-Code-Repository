5ed1b0674771d47cb73c6c839de4d16114593113
Dhruvnthakur <Dhruvnthakur@users.noreply.github.com>
2025-01-11 14:32:10 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -129,7 +129,7 @@ bool vote(int voter, int rank, string name)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (strcmp(candidates[i].name, name) == 0)
+        if (strcmp(name ,candidates[i].name) == 0)
         {
             preferences[voter][rank]=i;
             return true;
