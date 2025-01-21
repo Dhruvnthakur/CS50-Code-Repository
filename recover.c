1b0fa920e02820615f9e9cee4d6f5a3b4e2334db
Dhruvnthakur <Dhruvnthakur@users.noreply.github.com>
2025-01-21 14:18:29 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -1,7 +1,7 @@
-#include <stdbool.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[])
 {
@@ -12,51 +12,35 @@ int main(int argc, char *argv[])
     }
 
     // Open the memory card
-    FILE *card = fopen(argv[1], "r");
-
-    // Create a buffer for a block of data
-    uint8_t buffer[512];
-    bool found_jpeg = false;
-    int counter = 0;
-    char filename[8];
-    FILE *img = NULL;
-
-    // While there's still data left to read from the memory card
-    while (fread(buffer, 512, 1, card) == 512)
-    {
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff &&
-            (buffer[3] & 0xf0) == 0xe0)
-        {
-            if (found_jpeg)
-            {
-                fclose(img);
-            }
-            else
-            {
-                found_jpeg = true;
-            }
-
-            sprintf(filename, "%03d.jpg", counter);
-            img = fopen(filename, "w");
-            if (img == NULL)
-            {
-                fclose(card);
-                printf("Could not create %s. \n", filename);
-                return 3;
-            }
-            counter++;
-        }
-
-        if (found_jpeg)
-        {
-            fwrite(buffer, 521, 1, img);
-        }
-        // Create JPEGs from the data
-    }
-    fclose(card);
-    if (found_jpeg)
-    {
-        fclose(img);
-    }
-    return 0;
+    FILE *raw_file = fopen(argv[1], "r");
+uint8_t buffer[512];
+bool found_jpeg = false;
+int counter = 0;
+char filename [8];
+FILE *img = NULL;
+while (fread(buffer, 1, 512, raw_file))
+{
+if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+{
+found_jpeg = true;
+}
+if (found_jpeg == true)
+{
+if (counter != 0)
+{
+fclose(img);
+}
+sprintf(filename, "%03i.jpg", counter);
+img = fopen(filename, "w");
+fwrite(buffer, 1, 512, img);
+found_jpeg = false;
+counter++;
+}
+else if (counter != 0)
+{
+fwrite(buffer, 1, 512, img);
+}
+}
+fclose(img);
+fclose(raw_file);
 }
